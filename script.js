ae24bddc759f3c812775a3de88615bd7ae569c63
Salvatore <workstudioms@gmail.com>
2024-12-17 13:52:41 +0100

    js-biglietti-treno, assignment main structure

--- script.js
+++ script.js
@@ -1,7 +1,29 @@
+//ESERCIZIO DA SVOLGERE
+
 // Il programma dovrà chiedere all’utente il numero di chilometri che vuole percorrere e l’età del passeggero.
 // Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio, secondo queste regole:
-//- il prezzo del biglietto è definito in base ai km(0.21 € al km)
-//- va applicato uno sconto del 20 % per i minorenni
-//- va applicato uno sconto del 40 % per gli over 65.
-//- L’output del prezzo finale va messo fuori in forma umana(con massimo due decimali, per indicare centesimi sul prezzo).
+// - il prezzo del biglietto è definito in base ai km(0.21 € al km)
+// - va applicato uno sconto del 20 % per i minorenni
+// - va applicato uno sconto del 40 % per gli over 65.
+// - L’output del prezzo finale va messo fuori in forma umana(con massimo due decimali, per indicare centesimi sul prezzo).
 // Questo richiederà un minimo di ricerca.
+
+
+
+//PUNTI DELLA TRACCIA
+
+// - prompt , età del passeggero & numero di km da percorrere
+// - output , prezzo totale del viaggio, max 2 cifre decimali
+// - 0.21 € * km
+// - sconto 18- , 20%
+// - sconto 65+ , 40%
+
+
+
+//ESECUZIONE DELL'ESERCIZIO
+
+// Dichiarazione variabili , input
+
+// Funzione e condizioni, elaborazione
+
+// Risultato, output
