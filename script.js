9750a1c1d2071667fba8afa227bcfc7c41530f30
Salvatore <workstudioms@gmail.com>
2024-12-17 14:31:22 +0100

    js-biglietto-treno, complete

--- script.js
+++ script.js
@@ -23,7 +23,42 @@
 //ESECUZIONE DELL'ESERCIZIO
 
 // Dichiarazione variabili , input
+let distanceToTravel = parseInt(prompt("Inserisci il numero di chilometri da percorrere"));
+let userAge = parseInt(prompt("Inserisci la tua età"));
+let totalPrice;
 
-// Funzione e condizioni, elaborazione
+console.log(distanceToTravel, userAge);
+
+// Calcolo prezzo del biglietto esclusi sconti applicabili
+let ticketPrice = distanceToTravel * 0.21;
+
+console.log(ticketPrice);
+
+// Funzioni e condizioni, elaborazione
+
+if (userAge < 18) {
+
+    // Calcolo dello sconto applicabile
+    let discount = (ticketPrice * 20) / 100;
+
+    // Prezzo totale con sconto applicato
+    totalPrice = ticketPrice - discount;
+
+} else if (userAge >= 65) {
+
+    // Calcolo dello sconto applicabile
+    let discount = (ticketPrice * 40) / 100;
+
+    // Prezzo totale con sconto applicato
+    totalPrice = ticketPrice - discount;
+
+} else {
+    
+    // Se l'utente ha una età compresa tra i 18 e i 65 anni non verrà applicato alcuno sconto
+    totalPrice = ticketPrice;
+
+}
 
 // Risultato, output
+
+console.log(`Il costo totale del biglietto sarà di: ${totalPrice} €`);
